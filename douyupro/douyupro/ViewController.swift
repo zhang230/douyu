69551b71e70b187eee9c594b74ffa4ddf7c0872e
zsh <zsh@zshs-Mac.local>
2025-01-07 15:33:28 +0800

    code

---
+++ douyupro/douyupro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  douyupro
+//
+//  Created by zsh on 2025/1/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ douyupro/douyuproUITests/douyuproUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  douyuproUITestsLaunchTests.swift
+//  douyuproUITests
+//
+//  Created by zsh on 2025/1/7.
+//
+
+import XCTest
+
+final class douyuproUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
